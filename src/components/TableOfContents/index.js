8949ee2da786c5d2bad92c435949985a1a248e3f
Smembe812 <psemberekajr@gmail.com>
2020-03-09 17:51:38 +0100

    created ToCLink component

--- src/components/TableOfContents/index.js
+++ src/components/TableOfContents/index.js
@@ -1,20 +1,7 @@
 import React, {useState} from 'react';
-import MenuList from '@material-ui/core/MenuList';
-import MenuItem from '@material-ui/core/MenuItem';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import './toc.css'
-
-const useStyles = makeStyles({
-  root: {
-    width: 230,
-  },
-});
-
-
 
 function TableOfContents() {
-  const classes = useStyles();
+  //TODO: {function} to handle when clicking outside a component
 
   const handleOnClick = (e) => {
     const className = "toc__a toc__a--active"
@@ -29,46 +16,29 @@ function TableOfContents() {
   }
 
   return (
-      // <MenuList>
-      //   <MenuItem>
-      //       Description
-      //   </MenuItem>
-      //   <MenuItem>
-      //       Motivation
-      //   </MenuItem>
-      //   <MenuItem>
-      //       Steps
-      //   </MenuItem>
-      //   <MenuItem>
-      //     Related to
-      //   </MenuItem>
-      //   <MenuItem>
-      //     Resources
-      //   </MenuItem>
-      // </MenuList>
       <ul className="toc">
-        <li>
+        <li className="toc__item">
           <a href="#description" className="toc__a" onClick={handleOnClick}>Description</a>
-            <ul className="toc">
-              <li >
+            <ul className="toc__sub-toc">
+              <li className="toc__item">
                 <a href="#introduction" className="toc__a">Description</a>
               </li>
-              <li>
+              <li className="toc__item">
                   <a href="#introduction" className="toc__a">Motivation</a>
               </li>
             </ul>
         </li>
-        <li>
+        <li className="toc__item">
         <a href="#motivation" className="toc__a">Motivation</a>
             
         </li>
-        <li>
+        <li className="toc__item">
           <a href="#steps" className="toc__a">Steps</a>
         </li>
-        <li>
+        <li className="toc__item">
         <a href="#related-to" className="toc__a">Related to</a>
         </li>
-        <li>
+        <li className="toc__item">
           <a href="#resources" className="toc__a">Resources</a>
         </li>
       </ul>
