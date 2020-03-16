e94b1fde9d7d5e2cd29e32bc44019e28f8ba5fe1
Smembe812 <psemberekajr@gmail.com>
2020-03-16 20:36:49 +0100

    editor to JSX convertion passing

--- src/components/EditorToJSX/index.js
+++ src/components/EditorToJSX/index.js
@@ -1,8 +1,33 @@
 //Todo convert Editor JS data to JSX elements & content
+import React from 'react'
+
 import RenderList from './RenderList'
 import RenderListItem from './RenderListItem'
 import RenderHeader from './RenderHeader'
 import RenderParagraph from './RenderParagraph'
+
+function EditorToJSX({data, options, children}){
+    const {blocks} = data
+
+    const converted = blocks.map(block => {
+        const {type} = block
+        if (type === "header"){
+            return <RenderHeader block={block}/>
+        }
+        if (type === "list"){
+            return <RenderList block={block}/>
+        }
+        if (type === "paragraph"){
+            return  <RenderParagraph block={block}/>
+        }
+
+        return null
+    })
+
+    return converted
+}
+
+export default EditorToJSX
 
 export {
     RenderHeader, 
